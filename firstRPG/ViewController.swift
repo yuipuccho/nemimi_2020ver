64e2f398dcbd18e3944bf31dd0a30a1b2e5ada4e
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-15 17:53:12 +0900

    fistCommit

---
+++ firstRPG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstRPG
+//
+//  Created by 吉澤優衣 on 2019/08/15.
+//  Copyright © 2019 吉澤優衣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
