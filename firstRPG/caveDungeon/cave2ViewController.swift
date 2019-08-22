89e1db109f15e37e784f42d405c6a97a82ee2998
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-23 00:01:31 +0900

    2マップ目作成

---
+++ firstRPG/caveDungeon/cave2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  cave2ViewController.swift
+//  firstRPG
+//
+//  Created by 吉澤優衣 on 2019/08/22.
+//  Copyright © 2019 吉澤優衣. All rights reserved.
+//
+
+import UIKit
+
+class cave2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
