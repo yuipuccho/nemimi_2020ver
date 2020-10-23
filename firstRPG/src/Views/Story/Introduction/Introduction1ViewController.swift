b08ba01491e1c977df97d15a855dc43772a10022
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-10-24 00:07:28 +0900

    [Add] Introduction1VC.makeInstance

--- firstRPG/src/Views/Story/Introduction/Introduction1ViewController.swift
+++ firstRPG/src/Views/Story/Introduction/Introduction1ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  story1ViewController.swift
+//  Introduction1ViewController.swift
 //  firstRPG
 //
 //  Created by 吉澤優衣 on 2019/08/29.
@@ -8,51 +8,64 @@
 
 import UIKit
 
-class story1ViewController: UIViewController {
+class Introduction1ViewController: UIViewController {
     
-    @IBOutlet weak var princessImage: UIImageView!
-    @IBOutlet weak var hermitImage: UIImageView!
-    @IBOutlet weak var textView: UITextView!
-    
-    var count = 0
-    var button = false
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        hermitImage.isHidden = true
-        textView.isHidden = true
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.hermitImage.isHidden = false    // ハーミット登場
+//    @IBOutlet weak var princessImage: UIImageView!
+//    @IBOutlet weak var hermitImage: UIImageView!
+//    @IBOutlet weak var textView: UITextView!
+//    
+//    var count = 0
+//    var button = false
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        hermitImage.isHidden = true
+//        textView.isHidden = true
+//        
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+//            self.hermitImage.isHidden = false    // ハーミット登場
+//        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+//            self.textView.isHidden = false    // メッセージ表示
+//            self.textView.text = "＊「ホホホ......。」"
+//            self.button = true
+//        }
+//    }
+//    
+//    
+//    @IBAction func mainButton(_ sender: UIButton) {
+//        if button == true {
+//            switch count {
+//            case 0:
+//                count += 1
+//                textView.text = "姫「きゃあ！\n  どなたですか！？」"
+//            case 1:
+//                count += 1
+//                textView.text = "＊「さあ、わたしといっしょに\n  来てもらいますよ......。」"
+//            case 2:
+//                count += 1
+//                textView.text = "姫「きゃー！！」"
+//            case 3:
+//                performSegue(withIdentifier: "toStory2", sender: nil)
+//                
+//            default:
+//                return
+//            }
+//        }
+//    }
+}
+
+// MARK: - MakeInstance
+
+extension Introduction1ViewController {
+
+    static func makeInstance() -> UIViewController {
+        guard let vc = R.storyboard.introduction1ViewController.introduction1ViewController() else {
+            return UIViewController()
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-            self.textView.isHidden = false    // メッセージ表示
-            self.textView.text = "＊「ホホホ......。」"
-            self.button = true
-        }
+        return vc
     }
-    
-    
-    @IBAction func mainButton(_ sender: UIButton) {
-        if button == true {
-            switch count {
-            case 0:
-                count += 1
-                textView.text = "姫「きゃあ！\n  どなたですか！？」"
-            case 1:
-                count += 1
-                textView.text = "＊「さあ、わたしといっしょに\n  来てもらいますよ......。」"
-            case 2:
-                count += 1
-                textView.text = "姫「きゃー！！」"
-            case 3:
-                performSegue(withIdentifier: "toStory2", sender: nil)
-                
-            default:
-                return
-            }
-        }
-    }
+
 }
