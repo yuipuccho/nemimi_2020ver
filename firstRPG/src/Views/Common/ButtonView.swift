60190d24e0eb18f9961c438a687c54e1d947d34b
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-10-24 00:04:05 +0900

    [Add] ButtonView

--- firstRPG/src/Views/Common/ButtonView.swift
+++ firstRPG/src/Views/Common/ButtonView.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+/**
+ * 共通ボタンView
+ */
+class ButtonView: UIView {
+
+    // MARK: - Outlets
+
+    /// メインボタン
+    @IBOutlet weak var mainButton: UIButton!
+
+    // MARK: - Properties
+
+    // MARK: - LifeCycles
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    // MARK: - Functions
+
+    func subscribe() {
+        mainButton.rx.tap.subscribe(onNext: { [unowned self] in
+            
+        })
+    }
+
+}
+
+// MARK: - MakeInstance
+
+extension ButtonView {
+
+    static func makeInstance(owner: AnyObject? = nil, options: [UINib.OptionsKey: Any]? = nil) -> ButtonView {
+        return R.nib.buttonView.firstView(owner: owner, options: options)!
+    }
+
+}
