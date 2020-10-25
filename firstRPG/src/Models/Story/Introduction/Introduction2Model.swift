cbdfae97fdd1aa548e95ac00b2717fe3bab339b4
yui-yoshizawa <yui.yossy.0114@gmail.com>
2020-10-25 22:24:51 +0900

    [Add] Button tap

---
+++ firstRPG/src/Models/Story/Introduction/Introduction2Model.swift
@@ -0,0 +1,9 @@
+//
+//  Introduction2Model.swift
+//  firstRPG
+//
+//  Created by 吉澤優衣 on 2020/10/25.
+//  Copyright © 2020 吉澤優衣. All rights reserved.
+//
+
+import Foundation
--- firstRPG/src/Views/Common/ButtonView.swift
+++ firstRPG/src/Views/Common/ButtonView.swift
@@ -19,6 +19,16 @@ class ButtonView: UIView {
 
     /// メインボタン
     @IBOutlet weak var mainButton: UIButton!
+    /// 戻るボタン
+    @IBOutlet weak var backButton: UIButton!
+    /// 上ボタン
+    @IBOutlet weak var upButton: UIButton!
+    /// 左ボタン
+    @IBOutlet weak var leftButton: UIButton!
+    /// 右ボタン
+    @IBOutlet weak var rightButton: UIButton!
+    /// 下ボタン
+    @IBOutlet weak var downButton: UIButton!
 
     // MARK: - Properties
 
@@ -26,12 +36,16 @@ class ButtonView: UIView {
 
     /// メインボタンタップ
     var mainButtonTappedSubject: PublishSubject<Void> = PublishSubject<Void>()
-
-    // MARK: - LifeCycles
-
-    override class func awakeFromNib() {
-        super.awakeFromNib()
-    }
+    /// 戻るボタンタップ
+    var backButtonTappedSubject: PublishSubject<Void> = PublishSubject<Void>()
+    /// 上ボタンタップ
+    var upButtonTappedSubject: PublishSubject<Void> = PublishSubject<Void>()
+    /// 左ボタンタップ
+    var leftButtonTappedSubject: PublishSubject<Void> = PublishSubject<Void>()
+    /// 右ボタンタップ
+    var rightButtonTappedSubject: PublishSubject<Void> = PublishSubject<Void>()
+    /// 下ボタンタップ
+    var downButtonTappedSubject: PublishSubject<Void> = PublishSubject<Void>()
 
     // MARK: - Functions
 
@@ -39,6 +53,31 @@ class ButtonView: UIView {
         // メインボタンタップ
         mainButton.rx.tap.subscribe(onNext: { [unowned self] in
             self.mainButtonTappedSubject.onNext(())
+        }).disposed(by: disposeBag)
+
+        // 戻るボタンタップ
+        backButton.rx.tap.subscribe(onNext: { [unowned self] in
+            self.backButtonTappedSubject.onNext(())
+        }).disposed(by: disposeBag)
+
+        // 上ボタンタップ
+        upButton.rx.tap.subscribe(onNext: { [unowned self] in
+            self.upButtonTappedSubject.onNext(())
+        }).disposed(by: disposeBag)
+
+        // 左ボタンタップ
+        leftButton.rx.tap.subscribe(onNext: { [unowned self] in
+            self.leftButtonTappedSubject.onNext(())
+        }).disposed(by: disposeBag)
+
+        // 右ボタンタップ
+        rightButton.rx.tap.subscribe(onNext: { [unowned self] in
+            self.rightButtonTappedSubject.onNext(())
+        }).disposed(by: disposeBag)
+
+        // 下ボタンタップ
+        downButton.rx.tap.subscribe(onNext: { [unowned self] in
+            self.downButtonTappedSubject.onNext(())
         }).disposed(by: disposeBag)
     }
 
